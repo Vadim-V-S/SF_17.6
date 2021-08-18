ac4af4588b3c6eb7d5c98238a0fcfabbf1174472
Vadim-V-S <vaddimsav@gmail.com>
2021-08-18 16:57:50 +0500

    Добавьте файлы проекта.

---
+++ SF_17.6/Account.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SF_17._6
+{
+    class Account
+    {
+        // тип учетной записи
+        public string Type { get; set; }
+
+        // баланс учетной записи
+        public double Balance { get; set; }
+
+        // процентная ставка
+        public double Interest { get; set; }
+    }
+}
